f3b49d79cc7e848a05b5f9ef904a87214fbd8ea3
Sara <sara707ab@gmail.com>
2023-12-16 03:24:01 -0300

    refactor: simplificando métodos para encriptar y desencriptar.

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 let textoProcesado = document.getElementById("texto-procesado");
-            let textoProcesadoP = document.getElementById("texto-procesado-p");
-            let textoProcesadoOff = document.getElementById("texto-procesado-off");
-            let btnEncriptar = document.getElementById("btn-encriptar");
-            let btnDesenciptar = document.getElementById("btn-desencriptar");
-            let texto = document.getElementById("texto");
-            let btnCopiar = document.getElementById("btn-copiar");
+let textoProcesadoP = document.getElementById("texto-procesado-p");
+let textoProcesadoOff = document.getElementById("texto-procesado-off");
+let btnEncriptar = document.getElementById("btn-encriptar");
+let btnDesencriptar = document.getElementById("btn-desencriptar");
+let texto = document.getElementById("texto");
+let btnCopiar = document.getElementById("btn-copiar");
 
+const keys = [
+    [["e"],["enter"]],
+    [["i"],["imes"]],
+    [["a"],["ai"]],
+    [["o"],["ober"]],
+    [["u"],["ufat"]]
+];
+const keysLength = keys.length;
+
+//Hacer visible u ocultar un elemento.
 function displayOff(elemento){
     elemento.style.display = "none";
 }
@@ -14,110 +24,38 @@ function displayOn(elemento){
     elemento.style.display = "inline-block";
 }
 
+//Encriptar
 function encriptar(frase){
-    const fraseArray = frase.split("");
-    let fraseNueva = "";
-
-    for(let i=0;i<fraseArray.length;i++){
-        if(verificarVocal(fraseArray[i])){
-            switch(fraseArray[i]){
-                case "a":
-                    fraseNueva += "ai";
-                    break;
-                case "e":
-                    fraseNueva += "enter";
-                    break;
-                case "i":
-                    fraseNueva += "imes";
-                    break;
-                case "o":
-                    fraseNueva += "ober";
-                    break;
-                case "u":
-                    fraseNueva += "ufat";
-                    break;
-            }
-        }else{
-            fraseNueva += fraseArray[i];
+    for(let i = 0; i < keysLength; i++){
+        if(frase.includes(keys[i][0])){
+            frase = frase.replaceAll(keys[i][0], keys[i][1]);
         }
     }
-    return fraseNueva;
-}
-
-function verificarVocal(letra){
-    if(letra==="a" || letra==="e" || letra==="i" || letra==="o" || letra==="u"){
-        return true;
-    }else{
-        return false;
-    }
+    return frase;
 }
 
 function encriptarManejo(){
     let frase = encriptar(texto.value);
-    verificarTexto(frase);
+    controladorContainers(frase);
 }
 
+//Desencriptar
 function desencriptar(frase){
-    const fraseArray = frase.split("");
-    let fraseNueva = "";
-
-    for(let i=0;i<fraseArray.length;i++){
-        if(verificarVocal(fraseArray[i])){
-            switch(fraseArray[i]){
-                case "a":
-                    if("ai" === frase.substring(i,(i+2))){
-                        fraseNueva += "a";
-                        i++;
-                    }else{
-                        fraseNueva += fraseArray[i];
-                    }
-                    break;
-                case "e":
-                    if("enter" === frase.substring(i,(i+5))){
-                        fraseNueva += "e";
-                        i = i+4;
-                    }else{
-                        fraseNueva += fraseArray[i];
-                    }
-                    break;
-                case "i":
-                    if("imes" === frase.substring(i,(i+4))){
-                        fraseNueva += "i";
-                        i = i+3;
-                    }else{
-                        fraseNueva += fraseArray[i];
-                    }
-                    break;
-                case "o":
-                    if("ober" === frase.substring(i,(i+4))){
-                        fraseNueva += "o";
-                        i = i+3;
-                    }else{
-                        fraseNueva += fraseArray[i];
-                    }
-                    break;
-                case "u":
-                    if("ufat" === frase.substring(i,(i+4))){
-                        fraseNueva += "u";
-                        i = i+3;
-                    }else{
-                        fraseNueva += fraseArray[i];
-                    }
-                    break;
-            }
-        }else{
-            fraseNueva += fraseArray[i];
+    for(let i = 0; i <keysLength; i++){
+        if(frase.includes(keys[i][1])){
+            frase = frase.replaceAll(keys[i][1], keys[i][0]);
         }
     }
-    return fraseNueva;
+    return frase;
 }
 
 function desencriptarManejo(){
     let frase = desencriptar(texto.value);
-    verificarTexto(frase);
+    controladorContainers(frase);
 }
 
-function verificarTexto(frase){
+//Controlador de contenedores de los textos
+function controladorContainers(frase){
     if(frase === ""){
         displayOff(textoProcesado);
         displayOn(textoProcesadoOff);
@@ -128,6 +66,7 @@ function verificarTexto(frase){
     }
 }
 
+//Función para el botón copiar
 function copiar(){
     let inputNuevo = document.createElement("input");
     inputNuevo.value = textoProcesadoP.innerHTML;
@@ -139,6 +78,6 @@ function copiar(){
 
 displayOff(textoProcesado);
 
-            btnEncriptar.onclick = encriptarManejo;
-            btnDesenciptar.onclick = desencriptarManejo;
-            btnCopiar.onclick = copiar;
+btnEncriptar.onclick = encriptarManejo;
+btnDesencriptar.onclick = desencriptarManejo;
+btnCopiar.onclick = copiar;
